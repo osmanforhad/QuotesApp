8144dcb4ffebe0693ed260980b00c0e1244709b1
osman forhad <osmanforhad@pop-os.localdomain>
2020-04-17 20:06:58 +0600

    modify Mydapter java file as i need

--- app/src/main/java/com/osmanforhad/quotesapp/MyAdapter.java
+++ app/src/main/java/com/osmanforhad/quotesapp/MyAdapter.java
@@ -1,5 +1,46 @@
 package com.osmanforhad.quotesapp;
 
-public class MyAdapter {
+import android.view.View;
+import android.view.ViewGroup;
 
-}//end of the class
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    /* variable declaration **/
+    private String[] data;
+
+    /* constructor method for main class **/
+    public MyAdapter(String[] data) {
+        this.data = data;
+    }//end of the constructor method
+
+    /* onCreateViewHolder method for creating view **/
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }//end of the onCreateViewHolder method
+
+    /* onBindViewHolder method for Binding data with View **/
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }//end of the onBindViewHolder method
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }//end of the getItemCount method
+
+    /* Making Inner or sub Class for viewHolder **/
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }//end of the MyViewHolder constructor
+
+    }//end of the MyViewHolder Inner or Sub Class
+
+}//end of the class Main or Super Class
