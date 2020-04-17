4b1872b58e050e9397084b4f085e7b34a8a3c320
osman forhad <osmanforhad@pop-os.localdomain>
2020-04-17 19:40:34 +0600

    fresh application program

---
+++ app/src/main/java/com/osmanforhad/quotesapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.osmanforhad.quotesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//end of the onCreate method
+    
+}//end of the class
