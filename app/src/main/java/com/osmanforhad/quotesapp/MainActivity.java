5582579e3f0bccf8cc52f2265e377e70909c9ae2
osman forhad <osmanforhad@pop-os.localdomain>
2020-04-18 00:19:52 +0600

    writing code in MainActivity file as i need And solve The   65:20: AAPT: error: unescaped apostrophe in string error which was created for single quation sign into strings.xml file

--- app/src/main/java/com/osmanforhad/quotesapp/MainActivity.java
+++ app/src/main/java/com/osmanforhad/quotesapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.osmanforhad.quotesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -11,8 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* initial the string file
+         * where i was write my Quotes **/
+        String[] allQuotes = getResources().getStringArray(R.array.popularQuote);
+
         /* initial the recycler view **/
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        /* setup LayoutManager **/
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        /* setup Adapter which i was create
+        * name as
+        * MyAdapter**/
+        recyclerView.setAdapter(new MyAdapter(allQuotes));
 
     }//end of the onCreate method
 
